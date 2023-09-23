e37cf15830acb00de9378dd5755d6c655fe9dde9
Mamdouh <145754753+Mamdouh-Helmy@users.noreply.github.com>
2023-09-24 00:31:11 +0300

    Add files via upload

---
+++ JS/oneclick.js
@@ -0,0 +1,19 @@
+//link youtube
+function youtube() {
+    window.open("https://www.youtube.com/", "_blank");
+  }
+//link facebook
+  function facebook() {
+    window.open("https://www.facebook.com/");
+  }
+//link instagram
+  function instagram() {
+    window.open("https://www.instagram.com/");
+  }
+//scroll
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'// تمكين التمرير السلس
+    });
+  }
